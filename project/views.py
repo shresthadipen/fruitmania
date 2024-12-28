e41a14b977c4f72f04a056022df58b2d934b572c
dipen-shrestha <dipenshrestha227@gmail.com>
2024-12-28 17:09:52 +0545

    Update

--- project/views.py
+++ project/views.py
@@ -25,7 +25,7 @@ def login_view(request):
 
         if user is not None:
             login(request, user)
-            return HttpResponse ("Succesful")
+            return HttpResponse ("Login Succesful!!!")
 
         else:
             return HttpResponse("Invalid Username or Password")
